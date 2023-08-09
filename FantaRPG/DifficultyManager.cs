647fdae082d0ccda2d829f4885c271a408a4b3dc
Felnagy Dávid Zoltán <felnagyz@g4s.local>
2023-08-09 15:57:36 +0200

    idk

--- FantaRPG/DifficultyManager.cs
+++ FantaRPG/DifficultyManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FantaRPG.src;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,32 @@ namespace FantaRPG
 {
     internal class DifficultyManager
     {
+        private static DifficultyManager instance;
+        public static DifficultyManager Instance
+        {
+            get { return instance; }
+            set { instance = value; }
+        }
+        public readonly GameTime startTime;
+        double difficulty = 0;
+        private DifficultyManager(GameTime startTime)
+        {
+            this.startTime = startTime;
+            this.difficulty = 0;
+        }
+        public static DifficultyManager GetNewDifficultyManager(GameTime startTime)
+        {
+            DifficultyManager difficultyManager = new DifficultyManager(startTime);
+            return difficultyManager;
+        }
+        public void Update(GameTime gameTime)
+        {
+            difficulty += gameTime.GetElapsedSeconds();
+        }
+        public void LevelTransferHappened(double extraTimeinSeconds = 0)
+        {
+            difficulty += extraTimeinSeconds * 2.5;
+            difficulty *= 1.2;
+        }
     }
 }
